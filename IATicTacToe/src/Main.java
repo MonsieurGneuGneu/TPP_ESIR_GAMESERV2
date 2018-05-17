4153b86592e269eaa48cedf8d7fd2300d3b44aa7
cnabot <38343876+cnabot@users.noreply.github.com>
2018-05-17 17:16:49 +0200

    BetterWorkingAI

--- IATicTacToe/src/Main.java
+++ IATicTacToe/src/Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Main {
 	public static void main(String[] args){
 		int size=3;
@@ -7,42 +10,42 @@ public class Main {
 				table[i][j]=new Case(i,j);
 			}
 		}
-		
+
 		/*GAME LOOP*/
-		while(true){			
+		while(true){
 			System.out.println(printTable(table));
-			
+
 			/*AI 1 TURN*/
 			chooseCaseToFill(table,Fill.o).fill(Fill.o);
-			
+
 			System.out.println(printTable(table));
-			
+
 			long time = System.nanoTime();
 			while(System.nanoTime()<time+1000000000) ;//wait 1s
-			
+
 			//detect win
 			if(detectEnd(table))break;
-			
+
 			/*AI 2 TURN*/
 			chooseCaseToFill(table,Fill.x).fill(Fill.x);
-			
+
 			System.out.println(printTable(table));
-			
+
 			time = System.nanoTime();
 			while(System.nanoTime()<time+1000000000);//wait 1s
-			
+
 			//detect win
 			if(detectEnd(table))break;
 		}
-		
+
 	}
-	
+
 	private static boolean detectEnd(Case[][] table){
 		boolean flagfull = true;
-		
+
 		int countx = 0;
 		int counto = 0;
-		
+
 		/*row check*/
         for (Case[] aTable : table) {
             for (Case anATable : aTable) {
@@ -61,7 +64,7 @@ public class Main {
             countx = 0;
             counto = 0;
         }
-		
+
 		/*column check*/
 		for(int j = 0;j<table.length;++j){
 			for(int i = 0;i<table[j].length;++i){
@@ -79,7 +82,7 @@ public class Main {
 			countx = 0;
 			counto = 0;
 		}
-		
+
 		/*diag1 check*/
 		for(int i = 0;i<table.length;++i){
 			if(table[i][i].filledBy()==Fill.x)countx++;
@@ -95,7 +98,7 @@ public class Main {
 		}
 		countx=0;
 		counto=0;
-		
+
 		/*diag2 check*/
 		for(int i = 0;i<table.length;++i){
 			if(table[i][table.length-i-1].filledBy()==Fill.x)countx++;
@@ -109,17 +112,17 @@ public class Main {
 			System.out.println("o won!");
 			return true;
 		}
-		
+
 		//useless
 		countx=0;
 		counto=0;
-		
+
 		if(flagfull)System.out.println("no winner!");
 		return flagfull;
 	}
 
 	/**
-	 * 
+	 *
 	 * @param table
      * the table
 	 * @return
@@ -128,6 +131,7 @@ public class Main {
 	 */
 	private static Case chooseCaseToFill(Case[][] table, Fill myFill){
 		/* 1) checks if AI can win (plays if found)*/
+
         for (Case[] aTable : table) {
             for (Case anATable : aTable) {
                 if (anATable.filledBy() == Fill.blank) {
@@ -138,6 +142,7 @@ public class Main {
             }
         }
 		/* 2) checks if foe is winning on his next turn (plays if found)*/
+
         for (Case[] aTable : table) {
             for (Case anATable : aTable) {
                 if (anATable.filledBy() == Fill.blank) {
@@ -147,27 +152,48 @@ public class Main {
                 }
             }
         }
-		/* 3) plays somewhere else (method yet to define, temporary = last free place)*/
+        /* 3) checks if foe is able to create a fork on his next turn (plays if found)*/
+        for (Case[] aTable : table) {
+            for (Case anATable : aTable) {
+                if (anATable.filledBy() == Fill.blank) {
+                    if (hasToPlayHereToMakeAFork(anATable, table, myFill)) {
+                        return anATable;
+                    }
+                }
+            }
+        }
+        /* 4) checks if opponent is able to create a fork on his next turn (plays if found)*/
+        for (Case[] aTable : table) {
+            for (Case anATable : aTable) {
+                if (anATable.filledBy() == Fill.blank) {
+                    if (hasToPlayHereToPreventAFork(anATable, table, myFill)) {
+                        return anATable;
+                    }
+                }
+            }
+        }
+		/* 5) plays somewhere else (method yet to define, temporary = last free place)*/
 		Case playLoc = table[table.length/2][table[table.length/2].length/2];
 		if(playLoc.filledBy()==Fill.blank){
-			return playLoc;			
+			return playLoc;
 		}else{
 			return playSomewhere(table);
 		}
 	}
-	
+
 	private static Case playSomewhere(Case[][] table){
-		for(Case[] clist : table){
+	    ArrayList<Case> res = new ArrayList<Case>();
+	    for(Case[] clist : table){
 			for(Case c : clist){
-				if(c.filledBy()==Fill.blank)return c;
+				if(c.filledBy()==Fill.blank)res.add(c);
 			}
 		}
-		return null;
+		return res.get(new Random().nextInt(res.size()));
 	}
-	
+
 	/**
 	 * Computes if the AI has to play on a case to avoid losing
-	 * 
+	 *
 	 * @param _case
 	 * the case to check (assuming it's blank)
 	 * @param table
@@ -177,7 +203,7 @@ public class Main {
 	 */
 	private static boolean hasToPlayHereToDef(Case _case, Case[][] table, Fill myFill){
 		Fill oppFill = myFill==Fill.o?Fill.x:Fill.o;
-		
+
 		/*checking the diag1 if case is on diag1*/
 		int count = 0;
         if (checkDiag1(_case, table, oppFill, count)) return true;
@@ -192,7 +218,7 @@ public class Main {
             if (aTable[_case.col()].filledBy() == oppFill) ++count;
         }
 		if(count==table.length-1) return true;
-		
+
 		/*checking the row*/
 		count=0;
 		for(int i = 0;i<table.length;++i){
@@ -202,29 +228,11 @@ public class Main {
 
     }
 
-    private static boolean checkDiag2(Case _case, Case[][] table, Fill oppFill, int count) {
-        if(_case.col()+_case.row()==table.length-1){
-            for(int i = 0;i<table.length;++i){
-                if(table[i][table.length-i-1].filledBy()==oppFill)++count;
-            }
-            return count == table.length - 1;
-        }
-        return false;
-    }
 
-    private static boolean checkDiag1(Case _case, Case[][] table, Fill oppFill, int count) {
-        if(_case.col()==_case.row()){
-            for(int i = 0;i<table.length;++i){
-                if(table[i][i].filledBy()==oppFill)++count;
-            }
-            return count == table.length - 1;
-        }
-        return false;
-    }
 
     /**
 	 * Computes if the AI has to play on a case to win
-	 * 
+	 *
 	 * @param _case
 	 * the case to check (assuming it's blank)
 	 * @param table
@@ -247,7 +255,7 @@ public class Main {
             if (aTable[_case.col()].filledBy() == myFill) ++count;
         }
 		if(count==table.length-1) return true;
-		
+
 		/*checking the row*/
 		count=0;
 		for(int i = 0;i<table.length;++i){
@@ -256,10 +264,81 @@ public class Main {
         return count == table.length - 1;
 
     }
-	
+
+    private static boolean hasToPlayHereToMakeAFork(Case _case, Case[][] table, Fill myFill){
+        table[_case.row()][_case.col()].fill(myFill);
+
+	    /*number of way to win at the next turn*/
+        int count = 0;
+
+        count = getWinCount(table, myFill, count);
+        table[_case.row()][_case.col()].fill(Fill.blank);
+
+	    return count >= 2;
+    }
+
+
+    private static boolean hasToPlayHereToPreventAFork(Case _case, Case[][] table, Fill myFill){
+        Fill oppFill = myFill==Fill.o?Fill.x:Fill.o;
+        table[_case.row()][_case.col()].fill(oppFill);
+
+        /*number of way to win at the next turn*/
+        int count = 0;
+
+        count = getWinCount(table, oppFill, count);
+        table[_case.row()][_case.col()].fill(Fill.blank);
+
+        return count >= 2;
+    }
+
+    private static int getWinCount(Case[][] table, Fill oppFill, int count) {
+        for (Case[] aTable : table) {
+
+            for (Case anATable : aTable) {
+                if (anATable.filledBy() == Fill.blank) {
+                    if (hasToPlayHereToWin(anATable, table, oppFill)) {
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+
+
+    private static boolean checkDiag2(Case _case, Case[][] table, Fill oppFill, int count) {
+        if(_case.col()+_case.row()==table.length-1){
+            for(int i = 0;i<table.length;++i){
+                if(table[i][table.length-i-1].filledBy()==oppFill)++count;
+            }
+            return count == table.length - 1;
+        }
+        return false;
+    }
+
+    /**
+     * Assuming table has same width and height
+     *
+     * @param _case
+     * the case to check (assuming it's blank)
+     * @param table
+     * current state of the game
+     * @return
+     * true if this diag contains n-1 oppFill symbol
+     */
+    private static boolean checkDiag1(Case _case, Case[][] table, Fill oppFill, int count) {
+        if(_case.col()==_case.row()){
+            for(int i = 0;i<table.length;++i){
+                if(table[i][i].filledBy()==oppFill)++count;
+            }
+            return count == table.length - 1;
+        }
+        return false;
+    }
+
 	/**
 	 * Assuming table has same width and height
-	 * 
+	 *
 	 * @param table
 	 * a square table
 	 * @return
@@ -306,7 +385,7 @@ class Case{
 		this.col=col;
 		this.filledBy=Fill.blank;
 	}
-	
+
 	int row(){
 		return row;
 	}
