689ac314140e4ee28e825d635a280396ce1c15bb
Valentin Courtois <valentin.courtois.esir@gmail.com>
2018-05-28 16:51:20 +0200

    Remote Call Connect4 TODO

---
+++ TPP_ESIR_GAMESERV2_jhipster/src/main/java/com/mycompany/myapp/web/rest/connect4/RemoteCallConnect4.java
@@ -0,0 +1,11 @@
+package com.mycompany.myapp.web.rest.connect4;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+//@Secured(AuthoritiesConstants.ADMIN)
+@RequestMapping("/api")
+public class RemoteCallConnect4 {
+    //TODO
+}
