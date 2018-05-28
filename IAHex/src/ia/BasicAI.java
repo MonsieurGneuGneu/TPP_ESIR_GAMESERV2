4d9f9ea48a1587893275ef38a06c1c1e615f3bf3
0Kain <y12eg96@gmail.com>
2018-05-28 18:09:59 +0200

    IAHex second commit

--- IAHex/src/ia/BasicAI.java
+++ IAHex/src/ia/BasicAI.java
@@ -1,21 +1,110 @@
 package ia;
 
+import java.util.ArrayList;
+
 import ia.Main.Fill;
 
 public class BasicAI implements AI {
 	private int size;
 	private Fill myFill;
 	private Fill oppFill;
-	
-	BasicAI(int size, Fill myFill){
+
+	BasicAI(int size, Fill myFill) {
 		this.size = size;
 		this.myFill = myFill;
-		oppFill = myFill==Fill.r?Fill.b:Fill.r;
+		oppFill = myFill == Fill.r ? Fill.b : Fill.r;
 	}
-	
+
 	@Override
 	public void chooseCaseToFill(Case[][] table) {
-		
+
+		for (Case[] atable : table) {
+			for (Case antable : atable) {
+
+			}
+		}
 
 	}
+
+	/**
+	 * 
+	 * @param depth
+	 * @param table
+	 * @param testingFill
+	 * @return
+	 */
+	private Pair<Integer, Case> minimax(int depth, Case[][] table, Fill testingFill) {
+		ArrayList<Case> possiblemove = moveList(table, testingFill);
+
+		int bestScore = (myFill == testingFill) ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+		int currentScore;
+		Case bestMove = null;
+
+		if (depth == 0) {
+			bestScore = evaluate(table);
+		} else {
+			for (Case move : possiblemove) {
+				play(table, move, testingFill);
+				if (myFill == testingFill) {
+					currentScore = minimax(depth - 1, table, oppFill).getKey();
+					if (currentScore > bestScore) {
+						bestScore = currentScore;
+						bestMove = move;
+					}
+				} else {
+					currentScore = minimax(depth - 1, table, myFill).getKey();
+					if (currentScore < bestScore) {
+						bestScore = currentScore;
+						bestMove = move;
+					}
+				}
+
+				play(table, move, Fill.blank);
+			}
+		}
+		return new Pair<>(bestScore, bestMove);
+	}
+
+	/**
+	 * 
+	 * @param table
+	 * @param move
+	 * @param fill
+	 */
+	private void play(Case[][] table, Case move, Fill fill) {
+		if (move != null) {
+			table[move.row()][move.col()].fill(fill);
+		}
+	}
+
+	/**
+	 * 
+	 * @param table
+	 * @param fill
+	 * @return
+	 */
+	private ArrayList<Case> moveList(Case[][] table, Fill fill) {
+		ArrayList<Case> moveList = new ArrayList<>();
+		if (fill == myFill) {
+			for (int i = 0; i < table.length - 1; i++) {
+				for (int j = 0; j < table.length; j++) {
+					if (table[i][j].filledBy() == Fill.blank) {
+						moveList.add(table[i][j]);
+					}
+				}
+			}
+		}
+		return moveList;
+	}
+
+	/**
+	 * 
+	 * @param table
+	 * @return
+	 */
+	private int evaluate(Case[][] table) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 }
--- IAHex/src/ia/Main.java
+++ IAHex/src/ia/Main.java
@@ -5,34 +5,38 @@ import java.util.ArrayList;
 public class Main {
 
 	public static void main(String[] args) {
-		int size = 14;
+		int size = 3;
 		Case[][] table = new Case[size][size];
 		for (int i = 0; i < size; ++i) {
 			for (int j = 0; j < size; ++j) {
 				table[i][j] = new Case(i, j);
 			}
 		}
+		
+		//Le rouge essaye de lier une colonne
+		//Le bleu essaye de lier une ligne
+		
 		/* Initialisation of AI */
        
 
 		System.out.println(printTable(table));
 		/* GAME LOOP */
-		while (true) {
-
-
-			
-			/* AI V TURN */
-			
-			long time = System.nanoTime();
-			while(System.nanoTime()<time+1000000000) ;//wait 1s
-
-
-            /* AI H TURN */
-            
-            time = System.nanoTime();
-            while(System.nanoTime()<time+1000000000) ;//wait 1s
-			
-		}
+//		while (true) {
+//
+//
+//			
+//			/* AI V TURN */
+//			
+//			long time = System.nanoTime();
+//			while(System.nanoTime()<time+1000000000) ;//wait 1s
+//
+//
+//            /* AI H TURN */
+//            
+//            time = System.nanoTime();
+//            while(System.nanoTime()<time+1000000000) ;//wait 1s
+//			
+//		}
 
 	}
 
@@ -43,24 +47,38 @@ public class Main {
 
     private static String printTable(Case[][] table) {
 		StringBuilder res = new StringBuilder();
+		String indent = " ";
+		String haut = "";
+		for(int i = 0; i < table.length;i++) {
+			haut += " / "+ "\\ ";
+		}
 		
+		String bas = "";
+		for(int i = 0; i < table.length;i++) {
+			bas += " \\ / ";
+		}
+
+		res.append(indent+haut+"\n");
 		for (Case[] aTable : table) {
+			res.append(indent+"|");
 			for (Case anATable : aTable) {
 				switch (anATable.filledBy()) {
 				case r:
-					res.append(" r ");
+					res.append(" r  |");
 					break;
 				case b:
-					res.append(" b ");
+					res.append(" b  |");
 					break;
 				case blank:
-					res.append(" - ");
+					res.append(" -  |");
 					break;
 				default:
 					break;
 				}
 			}
 			res.append("\n");
+			res.append(indent+bas+ "\\"+"\n");
+			indent += "  ";
 		}
 		return res.toString();
 	}
